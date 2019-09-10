b75d0f159d8d1157f2a0842fb893186571705492
Flavio Mendes <sap.flavio@gmail.com>
2019-09-10 01:19:04 +0000

    Versão Update / Delete

--- webapp/controller/S0.controller.js
+++ webapp/controller/S0.controller.js
@@ -1,12 +1,105 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
+], function (Controller, MessageToast, MessageBox) {
 	"use strict";
 
 	return Controller.extend("ovly.odata.controller.S0", {
 		onInit: function () {
 			this._list = this.byId("list"); // sap.m.List
 			this._modelo = this.getOwnerComponent().getModel("fonte"); // v2.ODataModel
+		},
+
+		onSave: function (oEvent) {
+
+			function onSuccess(oProdutoCriado, resposta) {
+				MessageToast.show("Produto criado com ID" + oProdutoCriado.ID);
+			}
+
+			function onError(oError) {
+				MessageBox.error("Erro:" + oError.responseText);
+			}
+
+			// criar variavel oNovoProduto
+			// ID, Name, Description
+			var oNovoProduto = {};
+			oNovoProduto.ID = this.byId("produto_id").getValue();
+			oNovoProduto.Name = this.byId("produto_nome").getValue();
+			oNovoProduto.Description = this.byId("produto_descricao").getValue();
+
+			var oParameters = {
+				success: onSuccess,
+				error: onError
+			};
+			// Capturar oDataModel
+			// Chamar o .create
+			this._modelo.create("/Products", oNovoProduto, oParameters);
+		},
+		onDelete: function (oEvent) {
+
+			function onSuccess() {
+				MessageToast.show("Produto removido com sucesso"); //
+			}
+
+			function onError(oError) {
+				MessageBox.error("Erro:" + oError.responseText);
+			}
+
+			var oSettings = {
+				success: onSuccess,
+				error: onError
+			};
+
+			var oParameters = oEvent.getParameters();
+			var oListItem = oParameters.listItem; // nao eh funcao - sap.m.StandardListItem
+			var oListItemContext = oListItem.getBindingContext("fonte");
+			var sPath = oListItemContext.getPath();
+
+			this._modelo.remove(sPath, oSettings);
+		},
+
+		onUpdate: function (oEvent) {
+
+			function onSuccess() {
+				MessageToast.show("Produto atualizado com sucesso");
+			}
+
+			function onError(oError) {
+				MessageBox.error("Erro:" + oError.responseText);
+			}
+
+			// criar variavel oNovoProduto
+			// ID, Name, Description
+			var oNovoProduto = {};
+			oNovoProduto.ID = this.byId("produto_id").getValue();
+			oNovoProduto.Name = this.byId("produto_nome").getValue();
+			oNovoProduto.Description = this.byId("produto_descricao").getValue();
+
+			var oParameters = {
+				success: onSuccess,
+				error: onError
+			};
+
+			var sPath = this._modelo.createKey("Products", {
+				ID: oNovoProduto.ID
+			});
+
+			this._modelo.update("/" + sPath, oNovoProduto, oParameters);
+
+		},
+
+		onItemPress: function (oEvent) {
+			// alert("clicou no item");
+
+			var oParameters = oEvent.getParameters();
+			var oListItem = oParameters.listItem; // nao eh funcao - sap.m.StandardListItem
+			var oListItemContext = oListItem.getBindingContext("fonte");
+			// window._contexto ..= oListItemContext;
+
+			this.byId("produto_id").setValue(oListItemContext.getProperty("ID"));
+			this.byId("produto_nome").setValue(oListItemContext.getProperty("Name"));
+			this.byId("produto_descricao").setValue(oListItemContext.getProperty("Description"));
 		},
 
 		onSearch: function (oEvent) {
